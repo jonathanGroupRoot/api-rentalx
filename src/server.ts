466ba6dedf4c0678c6e6e1235bdb8cd8cc92dcda
Jonathan Vinicius Braz Silva <58049154+jonathanGroupRoot@users.noreply.github.com>
2021-09-23 12:01:55 -0300

    update docker

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,12 @@ import swaggerUi from 'swagger-ui-express';
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 
+import './database';
+
 const app = express();
 
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
-app.listen(3000, () => console.log('Servidor funcionando na 3000'));
+app.listen(3333, () => console.log('Servidor funcionando na 3333'));
